e95d4f5362e60a4d86e33858525484b147efcfef
Chemseddine0 <chamseddinexd@gmail.com>
2024-12-10 15:39:39 +0100

    done

--- resources/views/post/allblogs.blade.php
+++ resources/views/post/allblogs.blade.php
@@ -60,9 +60,15 @@
 ================================================== -->
 <div class="container">
 	<div class="mainheading">
-		<h1 class="sitetitle">welcome</h1>
+		<h1 class="sitetitle">Explore a World of Ideas – Discover Articles on Technology, Lifestyle, Travel, and More!
+
+		</h1>
 		<p class="lead">
-			 Bootstrap theme, medium style, simply perfect for bloggers
+			"Your One-Stop Destination for Insights on Technology, Health, Travel, and Lifestyle!"
+
+
+
+
 		</p>
 	</div>
 <!-- End Site Title
@@ -136,7 +142,7 @@
 			</a>
 			<div class="card-block">
 				<h2 class="card-title"><a href="post.html">{{Str::limit($Blogs->title,30,'....etc')}}</a></h2>
-				<h4 class="card-text">{!! Str::limit($Blogs->description,70,'....etc')!!}.</h4>
+				<h4 class="card-text">{{ Str::limit($Blogs->description,20,'....etc')}}.</h4>
 				<div class="metafooter">
 					<div class="wrapfooter">
 						<span class="meta-footer-thumb">
@@ -144,7 +150,7 @@
 						</span>
 						<span class="author-meta">
 						<span class="post-name"><a href="author.html">{{$Blogs->author}}</a></span><br/>
-						<span class="post-date">{!!date('D-M-Y', strtotime($Blogs->created_at))!!}</span><span class="dot"></span><span class="post-read"></span>
+						<span class="post-date">{{!date('D-M-Y', strtotime($Blogs->created_at))}}</span><span class="dot"></span><span class="post-read"></span>
 						</span>
 						<span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
 					</div>
